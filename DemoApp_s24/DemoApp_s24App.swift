e3d01411580b09dc7d74e998924b771c524bf409
Justin Pelletier <jpell3@unh.newhaven.edu>
2024-01-30 20:51:54 -0500

    Initial Commit

---
+++ DemoApp_s24/DemoApp_s24App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp_s24App.swift
+//  DemoApp_s24
+//
+//  Created by Justin Pelletier on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp_s24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
