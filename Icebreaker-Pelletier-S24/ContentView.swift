fb2976276f77c6dc23c65c35ea2a053282a02f79
Justin Pelletier <jpell3@unh.newhaven.edu>
2024-02-06 19:55:00 -0500

    Initial commit for Icebreaker Exercise

--- Icebreaker-Pelletier-S24/ContentView.swift
+++ Icebreaker-Pelletier-S24/ContentView.swift
@@ -8,15 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State var firstName: String = ""
+	@State var lastName: String = ""
+	@State var preferredName: String = ""
+	@State var answer: String = ""
+	@State var question: String = ""
+
     var body: some View {
+			
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+					Text("Icebreaker")
+						.font(.system(size: 40))
+						.bold()
+					Text("Built with SwiftUI")
+					TextField("First Name", text: $firstName)
+					TextField("Last Name", text: $lastName)
+					TextField("Preferred Name", text: $preferredName)
+					Button(action: {
+						setRandomQuestion() }) {
+						Text("Get a new random question")
+					}
+					Text(question)
+					TextField("Answer", text: $answer)
+					Button(action: {
+						writeStudentToFirebase() }) {
+						Text("Submit")
+						}
         }
+				.font(.largeTitle)
+				.multilineTextAlignment(.center)
         .padding()
+				.autocorrectionDisabled()
     }
+	
+	func setRandomQuestion() {
+		print("first name: \(firstName)")
+	}
+	
+	func writeStudentToFirebase() {
+		print("\(firstName) (\(preferredName)) \(lastName)")
+		print("\(answer)")
+	}
 }
 
 #Preview {
