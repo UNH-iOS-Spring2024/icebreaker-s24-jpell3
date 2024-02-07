e281227747c0ca5577ddf453a857ecbda0157c3c
Justin Pelletier <jpell3@unh.newhaven.edu>
2024-02-06 19:05:57 -0500

    Initial Commit

---
+++ Icebreaker-Pelletier-S24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Icebreaker-Pelletier-S24
+//
+//  Created by Justin Pelletier on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Icebreaker-Pelletier-S24/Icebreaker_Pelletier_S24App.swift
@@ -0,0 +1,17 @@
+//
+//  Icebreaker_Pelletier_S24App.swift
+//  Icebreaker-Pelletier-S24
+//
+//  Created by Justin Pelletier on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Icebreaker_Pelletier_S24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
